ea2fb8c041dc4213d0af4db51b4b82ddb50a5686
scchn <scchnxx@gmail.com>
2019-08-28 17:51:11 +0800

    init commit

---
+++ Freewaycam/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Freewaycam
+//
+//  Created by scchn on 2019/8/28.
+//  Copyright © 2019 scchn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Freewaycam/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Freewaycam
+//
+//  Created by scchn on 2019/8/28.
+//  Copyright © 2019 scchn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
